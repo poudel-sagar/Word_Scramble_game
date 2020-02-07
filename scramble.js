c0d379e1d60972316802f90919e182d8424a0ec7
Sagar <57942799+poudel-sagar@users.noreply.github.com>
2020-02-07 17:37:45 +0545

    Update scramble.js

--- scramble.js
+++ scramble.js
@@ -1,4 +1,3 @@
-
 <html>
 <head>
 
@@ -15,7 +14,7 @@ h1{
 text-align: center;
 color: white;
 font-size: 1.8rem;
-font-family:'Carter One',cursive;/*0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000*/
+font-family:'Carter One',cursive;
 text-transform: uppercase;
 letter-spacing: 15px;
 text-shadow: 0 1px 0 #efefef,
@@ -64,7 +63,33 @@ margin: 2rem 0;
 font-size: 1.1rem;
 }
 
+button{
+    font-size: 1rem;
+    cursor:pointer;
+    outline:none;
+    border:#2f3542;
+    margin-top: 10px;
+    color:#eb4d4b;
 
+}
+
+button.btn{
+    font-weight:600;
+    padding:1rem 2rem;
+    background:white;
+    text-transform:uppercase;
+    transition-property:all;
+    transition-duration: 0.5s;
+    transition-timing-function:cubic-bezier(0.65.-0.25,0.)
+}
+
+button.btn:hover,button.btn:focus, button.btn:active{
+    letter-spacing: 0.125rem;
+    word-spacing: 0.2rem;
+}
+.hideen{
+    display:none;
+}
 
 </style>
 </head>
@@ -83,9 +108,74 @@ font-size: 1.1rem;
 
         </div>
     </section>
+<script>
+    const msg=document.querySelector('.msg');
+    const guess=document.querySelector('input');
+    const btn=document.querySelector('.btn');
+    let play = false;
+    let newWords = "";
+    let randomWords="";
+    let sWords = ['python','javascript','c++','php','java','c#','html','css','reactjs','angular','swift','android','sql'];
+
+    const createNewWords = ()=>{
+        let ranNum = Math.floor(Math.random() * sWords.length);
+        console.log(random);
+        let newTemoSwords= sWords[ranNum];
+        console.log(newTempSwords.split(""));
+        return newTempSwords;
+
+    }
+const scrambleWords=(arr) =>{
+    for(let i=arr.length-1;i>0;i++){
+        let temp = arr[i];
+        console.log(temp);
+       let j=Math.floor(Math.random()*(i+1));
+       console.log(i);
+       console.log(j);
+       arr[i]=arr[j];
+       arr[j]=remp;
+    }
+    return arr;
+
+}
+
+    btn.addEventListener('click', function()
+    {
+        if(!play){
+            play=true;
+            btn.innerHTML = "GUESS";
+            guess.classList.toggle('hidden');
+            newWords=createNewWords();
+            randomWords=scrambleWords(newWords.split("")).join("");
+            console.log(randWords.join(""));
+            msg.innerHTML='Guess the word: ${randomWords}';
+
+
+        }else{
+            let tempWord=guess.value;
+            if(tempWord==newWords){
+                console.log('correct');
+                play =false;
+                msg.innerHTML='Awesome it is correct dude.it is $(randWords'};
+                btn.innerHTML="Start Again";
+                guess.classList.toggle('hidden');
+                guess.value="";
+
+            }
+            else{
+                console.log('incorrect');
+                msg.innerHTML = 'SOrry it is ${newWords}';
+
+            }
+        }
+
+    }
+
+</script>
+
+
 </body>
 
 
 
 </html>
-upto 14:39
